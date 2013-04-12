2abcda22dd10bc50b0035e7399edf625a28474c4
Justin Freels <jfreels@gmail.com>
2013-04-12 14:56:55 -0500

    tweaked to allocation selections in shiny

--- ui.r
+++ ui.r
@@ -9,7 +9,8 @@ reactiveSvg <- function (outputId)
 shinyUI(pageWithSidebar(
   headerPanel(title=HTML("Shiny and R adaptation of <a href = \"http://bl.ocks.org/4063663\">Mike Bostock's d3 Brushable Scatterplot</a>")),
   
-  sidebarPanel(
+  sidebarPanel(selectInput(inputId="selection",label="Select Funds",choices=sort(names(dat)[-(ncol(dat))]),multiple=TRUE),
+              HTML("<br>"),
               "This example explores the relationship of various Vanguard Funds representing different exposures.  Using Mike Bostock's
                interactive d3 scatterplot example, we can more thorougly discover the relationships between these funds.  Try it out by highlighting 
                portions of the scatterplot.  To remove the selection just click outside of the selection box.",
